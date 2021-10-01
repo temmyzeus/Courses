4dcb2ea17df4625a23b77172a776e24b6b280788
Temiloluwa Awoyele <awoyeletemiloluwa@gmail.com>
2021-10-01 15:54:59 +0100

    Update `sarcasm.py`

--- DeepLearning.AI TensorFlow Developer Professional Certificate/Natural Language Processing with Tensorflow/sarcasm.py
+++ DeepLearning.AI TensorFlow Developer Professional Certificate/Natural Language Processing with Tensorflow/sarcasm.py
@@ -1,5 +1,11 @@
+import os
 import ast
+from pprint import pprint
 
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+os.chdir('DeepLearning.AI TensorFlow Developer Professional Certificate/Natural Language Processing with Tensorflow')
 with open('./Sarcasm_Headlines_Dataset.json', mode='r') as f:
     data = f.readlines()
 
@@ -12,3 +18,22 @@ for line in data:
     article_links.append(line['article_link'])
     is_sarcastic.append(line['is_sarcastic'])
     headlines.append(line['headline'])
+
+tokenizer = Tokenizer(
+    num_words=None,
+    oov_token='<UNK>'
+)
+tokenizer.fit_on_texts(headlines)
+
+# Print Ordered Dict of Word Counts or Frequency
+print(tokenizer.word_counts, end='\n\n')
+
+# Number of docs tokens appeared in
+print(tokenizer.word_counts, end='\n\n')
+
+# Word Indeces, index is used to represent the word
+print(tokenizer.word_index, end='\n\n')
+
+pprint(headlines[:3])
+enc_tokens = tokenizer.texts_to_sequences(headlines[:3])
+pprint(enc_tokens)
